d6a82918061a520ea163fe3c8567a25a64f58eee
tommy10-code <mtywp058@gmail.com>
2025-08-18 11:42:39 +0900

    ruby復習

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,5 @@
-puts 11
+def chapter06_04(runtekun_profile)
+  # コードを記述
+  runtekun_profile.each { |key, value| "#{key}は、#{value}です。" }
+end
+puts chapter06_04({ name: 'らんてくん', birthday: '7月8日', tall: 80, weight: 25.6 })
