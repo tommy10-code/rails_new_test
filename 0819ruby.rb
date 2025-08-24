ecd28601339d8b3abe92bddfa112875f5d375e43
tommy10-code <mtywp058@gmail.com>
2025-08-24 21:39:25 +0900

    0824

--- 0819ruby.rb
+++ 0819ruby.rb
@@ -2,3 +2,30 @@ s = ["aaaaa", "bbbbbb", "cccc"]
 s.each do |a|
   puts a
 end
+
+n = "1234567891011"
+
+result = ""
+count = 0
+
+n.chars.reverse.each do |ch|
+    result << ch
+    count += 1
+    
+    if count % 3 == 0 && count != n.size
+        result << ","
+    end
+end
+
+puts result.reverse
+
+n = %i[3 1 4 2]
+min= n[0]
+
+n.each do |num|
+  if num < min
+    min = num
+  end
+end
+
+puts min
